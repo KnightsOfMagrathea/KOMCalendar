df74340692df975d75f467a69b3c6418c46c79bf
ambether <ambether@users.noreply.github.com>
2018-11-08 20:29:49 -0600

    Update KOMCalendar.lua

--- KOMCalendar/KOMCalendar.lua
+++ KOMCalendar/KOMCalendar.lua
@@ -1,8 +1,8 @@
-function mpac_OnLoad(self, event, ...)
+function KOMC_OnLoad(self, event, ...)
 	self:RegisterEvent("ADDON_LOADED")
 end
 
-function mpac_OnEvent(self, event, ...)
+function KOMC_OnEvent(self, event, ...)
 	if event == "ADDON_LOADED" and ... == "KOMCalendar" then
 		self:UnregisterEvent("ADDON_LOADED")
 	end
